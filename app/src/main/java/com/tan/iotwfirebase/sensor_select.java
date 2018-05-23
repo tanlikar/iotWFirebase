f89c7a8d795e19fc25db1884d56efd744b5b3942
tan <tanlikar@gmail.com>
2018-05-24 02:38:50 +0800

    improve scalability (unfinish)

---
+++ app/src/main/java/com/tan/iotwfirebase/sensor_select.java
@@ -0,0 +1,13 @@
+package com.tan.iotwfirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sensor_select extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_select);
+    }
+}
