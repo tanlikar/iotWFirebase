f29ea0d4c67d8a28c6617bd6586c35d4c3dba564
tan <tanlikar@gmail.com>
2018-11-15 15:30:15 +0800

    update

--- app/src/main/java/com/tan/iotwfirebase/Control.java
+++ app/src/main/java/com/tan/iotwfirebase/Control.java
@@ -1,6 +1,7 @@
 package com.tan.iotwfirebase;
 
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,7 +18,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.tan.iotwfirebase.Storage.AppPreferences;
 import com.tan.iotwfirebase.Storage.IPreferenceConstants;
 import com.tan.iotwfirebase.Storage.TinyDB;
 
@@ -27,13 +27,14 @@ import java.util.Objects;
 public class Control extends AppCompatActivity implements IPreferenceConstants {
 
     Switch mOn;
-    Switch mAuto;
+    Switch mAutoTemp;
     Switch mAutoGps;
+    Switch mAutoPmv;
     Button mUpButton, mDownButton;
     TextView mTextView;
 
     private Long mLedState;
-    private Long mAutoState;
+    private Long mAutoState, mAutoPmvState;
     private DatabaseReference mDatabaseReference;
     private TinyDB mTinyDB;
     private Long autoOnGps;
@@ -48,11 +49,12 @@ public class Control extends AppCompatActivity implements IPreferenceConstants {
         setContentView(R.layout.activity_control_new);
 
         mOn = (Switch) findViewById(R.id.on_switch);
-        mAuto = (Switch) findViewById(R.id.auto_switch);
+        mAutoTemp = (Switch) findViewById(R.id.auto_switch);
         mAutoGps = (Switch) findViewById(R.id.AutoGpsButton);
         mUpButton = (Button) findViewById(R.id.up_button);
         mDownButton = (Button) findViewById(R.id.down_button);
         mTextView = (TextView) findViewById(R.id.control_dis);
+        mAutoPmv = (Switch) findViewById(R.id.autoPMV);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_activity_control);
         setSupportActionBar(toolbar);
@@ -74,15 +76,15 @@ public class Control extends AppCompatActivity implements IPreferenceConstants {
         //appPreferences.putString(CHILD_KEY, groupNum);
 
         setInitTemp();
-        enableLed();
+        enableOn();
         enableAutoOnGps();
         checkAutoState();
-        checkLedState();
+        checkOnState();
         control();
 
     }
 
-    private void checkLedState(){
+    private void checkOnState(){
 
         mDatabaseReference.child(childlist.get(0)).child(childlist.get(1)).child("on").addValueEventListener(new ValueEventListener() {
             @Override
@@ -121,11 +123,11 @@ public class Control extends AppCompatActivity implements IPreferenceConstants {
 
                     if (mAutoState == 1){
 
-                        mAuto.setChecked(true);
+                        mAutoTemp.setChecked(true);
                         Log.d("Iot", "Auto on : on");
                     }else{
 
-                        mAuto.setChecked(false);
+                        mAutoTemp.setChecked(false);
                         Log.d("Iot", "Auto on : off");
                     }
                 }
@@ -136,9 +138,35 @@ public class Control extends AppCompatActivity implements IPreferenceConstants {
 
             }
         });
+
+        mDatabaseReference.child(childlist.get(0)).child(childlist.get(1)).child("auto_pmv").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.exists()) {
+                    mAutoPmvState = (Long) dataSnapshot.getValue();
+
+                    if (mAutoPmvState == 1) {
+
+                        mAutoPmv.setChecked(true);
+                        Log.d("Iot", "Auto on pmv : on");
+                    } else {
+
+                        mAutoPmv.setChecked(false);
+                        Log.d("Iot", "Auto on pmv: off");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
-    private void enableLed(){
+    private void enableOn(){
 
         mOn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -165,7 +193,7 @@ public class Control extends AppCompatActivity implements IPreferenceConstants {
             }
         });
 
-        mAuto.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        mAutoTemp.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
@@ -189,6 +217,31 @@ public class Control extends AppCompatActivity implements IPreferenceConstants {
                 }
 
 
+            }
+        });
+
+        mAutoPmv.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked){
+
+                    try {
+                        mDatabaseReference.child(childlist.get(0)).child(childlist.get(1)).child("auto_pmv").setValue(1);
+                        Log.d("Iot", "auto pmv : " + 1);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }else{
+
+                    try {
+                        mDatabaseReference.child(childlist.get(0)).child(childlist.get(1)).child("auto_pmv").setValue(0);
+                        Log.d("Iot", "auto pmv : " + 0);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
 
@@ -287,6 +340,7 @@ public class Control extends AppCompatActivity implements IPreferenceConstants {
             }
         });
     }
+
 
     @Override
     public void onBackPressed() {
